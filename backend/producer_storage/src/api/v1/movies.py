6082be5a1a58a15193ce2a60a4c1fb2ecdf56db2
xh4vm <h4vm@yandex.ru>
2022-12-01 23:46:12 +0500

    Возврат исключений при работе с некорректным токеном

--- backend/producer_storage/src/api/v1/movies.py
+++ backend/producer_storage/src/api/v1/movies.py
@@ -6,6 +6,7 @@ from fastapi import APIRouter, Depends, Body
 from src.containers.movie import ServiceContainer
 from src.services.producer.kafka import KafkaProducer
 from src.core.config import CONFIG
+from src.utils.error_dependency_handler import auth_dependency_handler
 
 from src.models.movie import MovieFrame
 
@@ -17,8 +18,9 @@ URL = f'{CONFIG.APP.HOST}:{CONFIG.APP.PORT}{CONFIG.APP.API_PATH}/{CONFIG.APP.API
 @router.post(path='/frame', name='Produce storage user data')
 @inject
 async def movie_frame(
-    frame: MovieFrame = Body(alias='movie_frame'),
-    sig: str = Body(),
+    _dependency_handler: None = Depends(auth_dependency_handler),
+    frame: MovieFrame = Body(title='MovieFrame', alias='movie_frame'),
+    sig: str = Body(title='Signature'),
     user_id: str = Depends(UserAccessRequired(permissions={URL: 'POST'})),
     kafka_producer: KafkaProducer = Depends(Provide[ServiceContainer.kafka_producer])
 ) -> None:
